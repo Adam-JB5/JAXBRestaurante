0a06b1d929b1713c8314a2d995533fc02dfdc5eb
unknown <DAM209@iesataulfo.local>
2025-11-21 19:43:29 +0100

    Cambios de POO de Ignacio

--- src/ejerciciojaxbrestaurante/EjercicioJAXBRestaurante.java
+++ src/ejerciciojaxbrestaurante/EjercicioJAXBRestaurante.java
@@ -22,83 +22,93 @@ public class EjercicioJAXBRestaurante {
      */
     public static void main(String[] args) {
         
+       
+        //Variables
         Restaurante restaurante = new Restaurante("Buen Llantar");
+        Menu menu;
+        Plato1 plato1;
+        Plato2 plato2;
+        Postre postre;
         
-        Plato1 plato1Garbanzos = new Plato1("caliente", "Garbanzos");
-        Plato1 plato1Lentejas = new Plato1("caliente", "Lentejas");
-        Plato1 plato1Salmorejo = new Plato1("frío", "Salmorejo");
-
-        Plato2 plato2Carne = new Plato2(180, "Carne");
-        Plato2 plato2Pescado = new Plato2(280, "Pescado");
-
-        Postre postreFlan = new Postre("si", "Flan");
-        Postre postreHelado = new Postre("no", "Helado");
-        Postre postreFruta = new Postre("no", "Fruta");
+        //Menu 1
+        menu = new Menu("lunes-viernes", 12);
         
-        Menu menu1 = new Menu("lunes-viernes", 12);
+        //Platos 1
+        plato1 = new Plato1("caliente", "Garbanzos");
+        menu.annadirPlatosYPostres(plato1);
+        plato1 = new Plato1("caliente", "Lentejas");
+        menu.annadirPlatosYPostres(plato1);
+        plato1 = new Plato1("frío", "Salmorejo");
+        menu.annadirPlatosYPostres(plato1);
         
-        menu1.annadirPlatosYPostres(plato1Garbanzos);
-        menu1.annadirPlatosYPostres(plato1Lentejas);
-        menu1.annadirPlatosYPostres(plato1Salmorejo);
+        //Platos 2
+        plato2 = new Plato2(180, "Carne");
+        menu.annadirPlatosYPostres(plato2);
+        plato2 = new Plato2(280, "Pescado");
+        menu.annadirPlatosYPostres(plato2);
         
-        menu1.annadirPlatosYPostres(plato2Carne);
-        menu1.annadirPlatosYPostres(plato2Pescado);
-        
-        menu1.annadirPlatosYPostres(postreFlan);
-        menu1.annadirPlatosYPostres(postreHelado);
-        menu1.annadirPlatosYPostres(postreFruta);
-        
-        restaurante.annadirMenu(menu1);
-        
-        
-        Menu menu2 = new Menu("sabado-domingo", 18);
-        
-        Plato1 plato1Escarola = new Plato1("frío", "Escarola");
-        Plato1 plato1Pisto = new Plato1("caliente", "Pisto");
-
-        Plato2 plato2Marisco = new Plato2(200, "Marisco");
-        Plato2 plato2Caza = new Plato2(220, "Caza");
-
-        Postre postreMouse = new Postre("si", "Mouse");
-        Postre postreCuajada = new Postre("no", "Cuajada");
-        
-        menu2.annadirPlatosYPostres(plato1Escarola);
-        menu2.annadirPlatosYPostres(plato1Pisto);
-        
-        menu2.annadirPlatosYPostres(plato2Marisco);
-        menu2.annadirPlatosYPostres(plato2Caza);
-        
-        menu2.annadirPlatosYPostres(postreMouse);
-        menu2.annadirPlatosYPostres(postreCuajada);
-        
-        restaurante.annadirMenu(menu2);
+        //Postres
+        postre = new Postre("si", "Flan");
+        menu.annadirPlatosYPostres(postre);
+        postre = new Postre("no", "Helado");
+        menu.annadirPlatosYPostres(postre);
+        postre = new Postre("no", "Fruta");
+        menu.annadirPlatosYPostres(postre);
         
         
-//        try {
-//            System.out.println("Creando el objeto JAXBContext e iniciándolo con la clase de la que se va a hacer marshalling");
-//            JAXBContext jc = JAXBContext.newInstance(Restaurante.class);
-//            System.out.println("Creando el objeto Marshaller y configurándolo para que genere salida XML formateada");
-//            Marshaller m = jc.createMarshaller();
-//            m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-//            System.out.println("Convirtiendo el objeto en un documento XML y almacenándolo en un fichero");
-//            File ficheroXML = new File(args[0]);
-//            m.marshal(restaurante, ficheroXML);
-//            System.out.println("Fichero XML generado");
-//        } catch (JAXBException ex) {
-//            System.out.println(ex);
-//        }
+        //Annadimos menu 1 a restaurante
+        restaurante.annadirMenu(menu);
         
-        File ficheroXML = new File(args[0]);
-
+        //Menu 2
+        menu = new Menu("kksabado-domingo", 18);
+        
+        //Platos 1
+        plato1 = new Plato1("kkcaliente", "Garbanzos");
+        menu.annadirPlatosYPostres(plato1);
+        plato1 = new Plato1("kkcaliente", "Lentejas");
+        menu.annadirPlatosYPostres(plato1);
+        
+        //Platos 2
+        plato2 = new Plato2(280, "kkPescado");
+        menu.annadirPlatosYPostres(plato2);
+        
+        //Postres
+        postre = new Postre("si", "kkFlan");
+        menu.annadirPlatosYPostres(postre);
+        postre = new Postre("no", "kkHelado");
+        menu.annadirPlatosYPostres(postre);
+        postre = new Postre("no", "kkFruta");
+        menu.annadirPlatosYPostres(postre);
+        
+        //Annadimos menu 2 a restaurante
+        restaurante.annadirMenu(menu);
+        
+        
         try {
+            System.out.println("Creando el objeto JAXBContext e iniciándolo con la clase de la que se va a hacer marshalling");
             JAXBContext jc = JAXBContext.newInstance(Restaurante.class);
-            Unmarshaller um = jc.createUnmarshaller();
-            Restaurante restaurante2 = (Restaurante) um.unmarshal(ficheroXML);
-            System.out.println("Terminado correctamente");
-            System.out.println(restaurante2);
-        } catch (JAXBException e) {
-            System.out.println(e);
+            System.out.println("Creando el objeto Marshaller y configurándolo para que genere salida XML formateada");
+            Marshaller m = jc.createMarshaller();
+            m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            System.out.println("Convirtiendo el objeto en un documento XML y almacenándolo en un fichero");
+            File ficheroXML = new File(args[0]);
+            m.marshal(restaurante, ficheroXML);
+            System.out.println("Fichero XML generado");
+        } catch (JAXBException ex) {
+            System.out.println(ex);
         }
+        
+//        File ficheroXML = new File(args[0]);
+//
+//        try {
+//            JAXBContext jc = JAXBContext.newInstance(Restaurante.class);
+//            Unmarshaller um = jc.createUnmarshaller();
+//            Restaurante restaurante2 = (Restaurante) um.unmarshal(ficheroXML);
+//            System.out.println("Terminado correctamente");
+//            System.out.println(restaurante2);
+//        } catch (JAXBException e) {
+//            System.out.println(e);
+//        }
     }
     
 }
